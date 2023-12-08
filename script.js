421666df3c2f1d8ffa65bca823d69f4f2e253c1b
Cristian Mattos <61815268+cristianmattos@users.noreply.github.com>
2023-12-08 20:09:52 -0300

    scroll animation

--- script.js
+++ script.js
@@ -53,3 +53,26 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+const sections = document.querySelectorAll(".js-scroll");
+
+function initAnimacaoScroll(){
+if (sections.length) {
+  const windowMetade = window.innerHeight * 0.6;
+
+  function animarScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowMetade < 0;
+      if (isSectionVisible) section.classList.add("ativo");
+      else section.classList.remove("ativo");
+    });
+  }
+
+  animarScroll();
+
+  window.addEventListener("scroll", animarScroll);
+}
+}
+
+initAnimacaoScroll();
