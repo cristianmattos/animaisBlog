d15800706783c8abc927e892b27baa418f4179a7
Cristian Mattos <61815268+cristianmattos@users.noreply.github.com>
2024-01-04 12:27:34 -0300

    add module

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import initTabNav from "./modules/tabnav.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+
+initTabNav();
+initAnimacaoScroll();
+initAccordion();
