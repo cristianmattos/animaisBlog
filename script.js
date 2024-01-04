d7a5d6ad28136a8ef4e1a71459bfc07d17c920b5
Cristian Mattos <61815268+cristianmattos@users.noreply.github.com>
2024-01-04 11:32:08 -0300

    update

--- script.js
+++ script.js
@@ -56,23 +56,23 @@ initAccordion();
 
 const sections = document.querySelectorAll(".js-scroll");
 
-function initAnimacaoScroll(){
-if (sections.length) {
-  const windowMetade = window.innerHeight * 0.6;
+function initAnimacaoScroll() {
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
 
-  function animarScroll() {
-    sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - windowMetade < 0;
-      if (isSectionVisible) section.classList.add("ativo");
-      else section.classList.remove("ativo");
-    });
+    function animarScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
+    }
+
+    animarScroll();
+
+    window.addEventListener("scroll", animarScroll);
   }
-
-  animarScroll();
-
-  window.addEventListener("scroll", animarScroll);
-}
 }
 
 initAnimacaoScroll();
