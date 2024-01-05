aa8792e08968413ad49f1e270662e6b64c621e6e
Cristian Mattos <61815268+cristianmattos@users.noreply.github.com>
2024-01-05 18:50:28 -0300

    dropdown start

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
-
-
+import initDropdownMenu from "./modules/dropdown-menu.js";
 
 initTabNav();
 initAnimacaoScroll();
 initAccordion();
 initModal();
 initTooltip();
+initDropdownMenu();
