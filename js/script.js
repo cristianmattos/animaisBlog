44baadcbc40a3b83748ed53c57d6bec7112e4479
Cristian Mattos <61815268+cristianmattos@users.noreply.github.com>
2024-01-05 10:49:06 -0300

    finished modal

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 import initTabNav from "./modules/tabnav.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
+import initModal from "./modules/modal.js";
+
 
 initTabNav();
 initAnimacaoScroll();
 initAccordion();
+initModal();
