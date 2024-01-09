f49f71b3642de910d55e1c96c8875177eea93bc3
Cristian Mattos <61815268+cristianmattos@users.noreply.github.com>
2024-01-09 16:33:47 -0300

    operating time function completed

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from './modules/menu-mobile.js'
 import initAnimaNumeros from './modules/anima-numeros.js'
-
+import initFuncionamento from "./modules/funcionamento.js";
 
 
 initTabNav();
@@ -17,3 +17,4 @@ initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initAnimaNumeros();
+initFuncionamento();
